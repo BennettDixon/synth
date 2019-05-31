23857aa033d05b7fa99efac9b8fb29007f1e9797
jmgindi <jmgindi@gmail.com>
2019-05-31 17:35:01 +0000

    continuing work on synth.py file

--- synth.py
+++ synth.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 import click
+import os
+import shutil
 
 @click.command()
+@click.option("--name",
+              default="my_project",
+              help="name of your project")
 @click.option("--frontend",
               default="static",
               help="frontend to use")
@@ -11,9 +16,25 @@ import click
 @click.option("--database",
               default=None,
               help="database to use")
-def synth(frontend, backend, database):
+def synth(name, frontend, backend, database):
     """ creates a synth wireframe with your desired frontend,
     backend, and database
     """
+    allowed_front = ["static", "node", "react"]
+    allowed_back = ["node", "flask", "django"]
+    allowed_db = ["mysql", "postgres", "mongodb"]
+
+    if not os.path.exists(name):
+        os.mkdir(name)
+    else:
+        raise FileExistsError('Directory {} exists.'.format(name))
+    
+    if frontend == "static":
+        os.mkdirs("{}/nginx_router/frontend/static".format(name))
+        os.mkdir("{}/nginx_router/nginx_conf")
+        shutil.copyfile("/usr/bin/synth/master/index.html",
+                        "{}/nginx_router/frontend/static/index.html"
+                        .format(name))
     print(frontend)
     print('hi')
+    
