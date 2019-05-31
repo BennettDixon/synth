5afe32cdc9b64a3d848b549faf24acab816c82a9
Bennett Dixon <benbendixon@gmail.com>
2019-05-31 15:16:04 -0700

    fixed some typos in the install script, non impactful

--- install.sh
+++ install.sh
@@ -7,12 +7,12 @@ echo 'Python 3.4+ is required to run synth!'
 # python3 dependencies
 pip3 install click
 
-echo 'Installing bootstrap projects...'
 # setup content in /etc
+echo 'Installing bootstrap projects in /etc/synth...'
 mkdir -p /etc/synth
 cp -R ./projects_master/ /etc/synth/projects_master/
 
-echo 'Creating executable in /usr/bin...'
-# setup executable in /usr/bin
+# setup executable in /usr/local/bin
+echo 'Creating executable in /usr/local/bin...'
 cp ./synth.py /usr/local/bin/synth
 chmod +x /usr/local/bin/synth
