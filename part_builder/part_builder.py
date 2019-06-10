479b9e33b3dca3919343c57d25ca0a5d747e5ec4
jmgindi <jmgindi@gmail.com>
2019-06-10 18:27:26 +0000

    updated supported lists for partbuilder

--- part_builder/part_builder.py
+++ part_builder/part_builder.py
@@ -21,7 +21,7 @@ class PartBuilder():
     allowed_frontends = ['static', 'dynamic', 'react', 'reactjs']
     allowed_backends = ['node', 'flask', 'django']
     allowed_databases = ['mongo', 'postgres', 'mysql', 'mariadb']
-    allowed_caches = ['redis', 'memcache']
+    allowed_caches = ['redis', 'memcached']
 
     def __init__(self, parts_root=None, nginx_file=None, compose_file=None, front_enabled=False, back_enabled=False):
         """
