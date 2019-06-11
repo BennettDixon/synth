c04a7f0f13d1038df4b96e23e4c51bc5f071014b
Bennett Dixon <benbendixon@gmail.com>
2019-06-11 11:34:44 -0700

    node production upgrades and added post route for testing

---
+++ projects_master/nginx_router/backend/node/keys.js
@@ -0,0 +1,6 @@
+// Just some example configuration data for redis and postgres
+// Can be deleted without any effect
+module.exports = {
+  exampleData: process.env.MY_ENV_VARIABLE,
+  someMoreData: process.env.MORE_ENV_DATA
+};
